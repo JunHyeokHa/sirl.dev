539fc87aaa501748510b445424bf3f7011ecfb92
Sirl <sirl0@naver.com>
2020-12-07 15:28:21 +0900

    Update gatsby-meta-config.js

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -26,5 +26,5 @@ module.exports = {
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
   },
-  ga: 'G-1DJN48LKMW', // Add your google analytics tranking ID
+  ga: 'UA-184876368-1', // Add your google analytics tranking ID
 }
